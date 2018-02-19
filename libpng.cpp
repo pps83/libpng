04be23e9b28ecc2897e3703c70b48e788fe6db15
Pavel P <pavlov.pavel@gmail.com>
2018-02-18 17:55:35 -0800

    intel optimizations

--- libpng.cpp
+++ libpng.cpp
@@ -1,6 +1,9 @@
 #ifdef _MSC_VER
 #pragma warning(disable: 4996)
 #endif
+
+#define PNG_INTEL_SSE
+
 #include "png.c"
 #include "pngerror.c"
 #include "pngget.c"
@@ -16,6 +19,9 @@
 #include "pngwrite.c"
 #include "pngwtran.c"
 #include "pngwutil.c"
+
+#include "intel/intel_init.c"
+#include "intel/filter_sse2_intrinsics.c"
 
 #ifdef _WINDLL
 #include "zlib.cpp"
