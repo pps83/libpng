c9d3b2bad6c881c464028d64083945b1e1c10256
Pavel P <pavlov.pavel@gmail.com>
2018-02-27 17:19:24 -0800

    arm config

--- libpng.cpp
+++ libpng.cpp
@@ -2,7 +2,13 @@
 #pragma warning(disable: 4996)
 #endif
 
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
 #define PNG_INTEL_SSE
+#endif
+
+#if defined(__aarch64__) || defined(_M_ARM64) || defined(__arm__) || defined(_M_ARM)
+#define PNG_ARM_NEON_OPT 2
+#endif
 
 #include "png.c"
 #include "pngerror.c"
@@ -20,8 +26,15 @@
 #include "pngwtran.c"
 #include "pngwutil.c"
 
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
 #include "intel/intel_init.c"
 #include "intel/filter_sse2_intrinsics.c"
+#endif
+
+#if defined(__aarch64__) || defined(_M_ARM64) || defined(__arm__) || defined(_M_ARM)
+#include "arm/arm_init.c"
+#include "arm/filter_neon_intrinsics.c"
+#endif
 
 #ifdef _WINDLL
 #include "zlib.cpp"
